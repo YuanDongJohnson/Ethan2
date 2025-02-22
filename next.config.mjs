fe25b0afd20f4c1373fafb8125a5c8658c239e6f
YuanDongJohnson <163552945+YuanDongJohnson@users.noreply.github.com>
2025-02-22 23:45:15 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,13 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  distDir: "out",
   images: {
     unoptimized: true,
+    domains: ["img.picgo.net"],
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
+  trailingSlash: true,
 }
 
-mergeConfig(nextConfig, userConfig)
+module.exports = nextConfig
 
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
